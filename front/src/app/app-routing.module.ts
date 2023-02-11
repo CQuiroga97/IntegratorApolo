09d7342a6474e5c061a6084f2f45b99bf0600ac7
Canartovi97 <65434562+Canartovi97@users.noreply.github.com>
2023-02-11 11:25:07 -0500

    down bottoms

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 const routes: Routes = [
-  {path:"", component:InicioComponent}
+  {path:"", component:InicioComponent},  
 ];
 
 @NgModule({
