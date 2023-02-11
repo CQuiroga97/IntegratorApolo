5ac8f1163e22373c55ed97ac38e23e3e2b9bb99c
CQuiroga97 <quiroga.cristian97@hotmail.com>
2023-02-11 17:09:11 -0500

    Creación backend registro de universidades

--- back/routes/router.js
+++ back/routes/router.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const loginAdmin = require("../controllers/loginAdminController.js")
+const regUni = require("../controllers/registrarUniversidad.js")
 const router = express.Router();
 /* const {signUpValidation} */
 const bcrypt = require("bcrypt")
@@ -7,8 +8,10 @@ const bcrypt = require("bcrypt")
 //https://www.tutsmake.com/login-rest-api-in-node-js-with-mysql/
 //https://codingpotions.com/angular-login-sesion
 router.post("/login", (req, res, next) =>{
-    console.log(req.body)
     loginAdmin.login(req, res)
+})
+router.post("/registrarUniversidad", (req, res, next) =>{
+    regUni.registrarUniversidad(req, res);
 })
 
 module.exports = router;
