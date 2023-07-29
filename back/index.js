5ada3e1f1a5cc7f0ade1f4f4431fb82c810c49d4
CQuiroga97 <quiroga.cristian97@hotmail.com>
2023-07-29 18:08:38 -0500

    Prueba

--- back/index.js
+++ back/index.js
@@ -34,7 +34,7 @@ httpServer.listen(port, () => {
   console.log(`http://localhost:${port}`);
 });*/
 var http = require('http')
-var port = process.env.PORT || 1337;
+var port = process.env.PORT || 80;
 http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
