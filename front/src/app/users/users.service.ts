b38d2db276b14d013f78a654e137779140f43404
CQuiroga97 <quiroga.cristian97@hotmail.com>
2023-07-30 13:25:39 -0500

    Esta si

--- front/src/app/users/users.service.ts
+++ front/src/app/users/users.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs/observable";
+import { Observable } from "rxjs";
 import { CookieService } from "ngx-cookie-service";
 import { Router } from '@angular/router';
 import { AuthService } from "../auth/auth.service";
