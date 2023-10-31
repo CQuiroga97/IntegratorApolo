01c4cc99bee19017029cdc560688a8d36d50d90c
Canartovi97 <65434562+Canartovi97@users.noreply.github.com>
2023-10-30 19:00:28 -0500

    Cambios en el front, se agrega el footer

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -52,6 +52,7 @@ import { IntegralesRonda2Component } from './integrales-ronda2/integrales-ronda2
 import { PanelEliminatoriasComponent } from './admin/panel-eliminatorias/panel-eliminatorias.component';
 import { StreamSegundaRondaComponent } from './stream-segunda-ronda/stream-segunda-ronda.component';
 import { SegundaRondaComponent } from './segunda-ronda/segunda-ronda.component';
+import { FooterComponent } from './footer/footer.component';
 const config: SocketIoConfig = { url: 'https://integratorapi.azurewebsites.net', options: {} };
 /* const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} }; */
 @NgModule({
@@ -86,6 +87,7 @@ const config: SocketIoConfig = { url: 'https://integratorapi.azurewebsites.net',
     PanelEliminatoriasComponent,
     StreamSegundaRondaComponent,
     SegundaRondaComponent,
+    FooterComponent,
 
 
   ],
